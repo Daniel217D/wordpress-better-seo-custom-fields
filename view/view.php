129a5ac02e5458b574e0c3e1e552087e71fc08a0
Daniil <daniel217032001@gmail.com>
2019-11-27 22:45:36 +0300

    Update view.php

--- view/view.php
+++ view/view.php
@@ -1,45 +1,39 @@
-<?php 
-	class scfClassView
-	{
-		static function scf_add_taxseo_head_meta_fields()
-		{
-			if(is_category() || self::check_woocommerce() )
-			{
-				$term = get_queried_object();
-				$description = get_term_meta( $term->term_id, 'description', true );
-				$keywords = get_term_meta( $term->term_id, 'keywords', true );
-				if(!empty($description))
-				{
-					echo '<meta name="description" content="'.esc_html( $description ).'">'. "\n";
-				}
-				if(!empty($keywords))
-				{
-					echo '<meta name="keywords" content="'.esc_html( $keywords ).'">'. "\n";
-				}
-			}
-			else if(is_tag())
-			{
-				$term = get_queried_object();
-				$description = get_metadata('term', $term->term_id, 'description', 1 );
-				$keywords = get_metadata('term', $term->term_id, 'keywords', 1 );
-				
-				if(!empty($description))
-				{
-					echo '<meta name="description" content="'.esc_html( $description ).'">'. "\n";
-				}
-				if(!empty($keywords))
-				{
-					echo '<meta name="keywords" content="'.esc_html( $keywords ).'">'. "\n";
-				}
-			}
-		}
+<?php
+class scfClassView
+{
+    static function scf_add_taxseo_head_meta_fields()
+    {
+        $term = '';
+        $description = '';
+        $keywords = '';
+
+        if(is_category() || self::check_woocommerce() )
+        {
+            $term = get_queried_object();
+            $description = get_term_meta( $term->term_id, 'description', true );
+            $keywords = get_term_meta( $term->term_id, 'keywords', true );
+        }
+        else if(is_tag())
+        {
+            $term = get_queried_object();
+            $description = get_metadata('term', $term->term_id, 'description', 1 );
+            $keywords = get_metadata('term', $term->term_id, 'keywords', 1 );
+        }
+
+        if(!empty($description))
+        {
+            echo '<meta name="description" content="'.esc_html( $description ).'">'. "\n";
+        }
+        
+        if(!empty($keywords))
+        {
+            echo '<meta name="keywords" content="'.esc_html( $keywords ).'">'. "\n";
+        }
+    }
 
 
-		public static function check_woocommerce()
-		{
-			return (class_exists( 'WooCommerce' ) && is_product_category());
-		}
-	}
-
-
-?>
+    public static function check_woocommerce()
+    {
+        return (class_exists( 'WooCommerce' ) && is_product_category());
+    }
+}
